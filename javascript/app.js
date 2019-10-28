76ba9d53276d8d4a727e4164931c01a1b434ce63
TianQin <qintian29@gmail.com>
2019-10-28 15:38:18 +0800

    completed gif state change upon clicks, enabled API for both Giphy and imdb

--- javascript/app.js
+++ javascript/app.js
@@ -58,9 +58,71 @@ function searchGifandMovie(anyCity) {
     console.log(response);
     for (let i = 0; i < 10; i++) {
       var eachGiphy = $("<img>");
+      var rating = $("<p>");
       eachGiphy.addClass("each-giphy");
-      eachGiphy.attr("src", response.data[i].images.fixed_height.url);
-      $(".giphy").prepend(eachGiphy);
+      eachGiphy.attr("src", response.data[i].images.fixed_height_still.url);
+      eachGiphy.attr("data-state", "still");
+      eachGiphy.attr(
+        "data-still",
+        response.data[i].images.fixed_height_still.url
+      );
+      eachGiphy.attr("data-animate", response.data[i].images.fixed_height.url);
+      eachGiphy.click(function() {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      });
+
+      rating.addClass("each-rating");
+      rating.text("RATING: " + response.data[i].rating);
+
+      $(".giphy")
+        .prepend(rating)
+        .prepend(eachGiphy);
     }
+  });
+
+  let movieName = anyCity;
+  var queryUrlMovie =
+    "https://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
+  $.ajax({
+    url: queryUrlMovie,
+    method: "GET"
+  }).then(response => {
+    console.log(response);
+    var movieImg = $("<img>");
+    movieImg.addClass("movie-photo");
+    movieImg.attr("src", response.Poster);
+    var movieTitle = $("<h1>");
+    movieTitle.addClass("movie-title");
+    movieTitle.text(response.Title);
+    console.log(response.Poster);
+    var movieDetail = $("<p>");
+    movieDetail.addClass("movie-detail");
+    movieDetail.html(
+      "Country: " +
+        response.Country +
+        "<br>" +
+        "Director: " +
+        response.Director +
+        "<br>" +
+        "Production: " +
+        response.Production +
+        "<br>" +
+        "Year: " +
+        response.Year +
+        "<br>" +
+        "imdbRating: " +
+        response.imdbRating
+    );
+    $(".movie")
+      .prepend(movieDetail)
+      .prepend(movieTitle)
+      .prepend(movieImg);
   });
 }
